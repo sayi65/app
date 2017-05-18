99432695589c4de36c64dcbbd34a7a72ae1b61dc
cui@isb.co.jp <cui@isb.co.jp>
2017-05-18 16:35:09 +0900

    first

---
+++ app/src/main/java/com/app/isb_bs2/bs/MainApplication.java
@@ -0,0 +1,21 @@
+package com.app.isb_bs2.bs;
+
+import android.app.Application;
+
+import io.realm.Realm;
+import io.realm.RealmConfiguration;
+
+/**
+ * Created by sayi65 on 2017/04/20.
+ */
+
+public class MainApplication extends Application {
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        Realm.init(this);
+        RealmConfiguration realmConfiguration = new RealmConfiguration.Builder().build();
+        Realm.setDefaultConfiguration(realmConfiguration);
+    }
+}
